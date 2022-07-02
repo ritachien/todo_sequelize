0066fe497a20740ae1d5517f93a9fd3393f09e03
Rita Chien <ritachien929@gmail.com>
2022-07-02 12:52:59 +0800

    feat: add checkAuth middleware to routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const { checkAuth } = require('../middlewares/auth')
 const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
 
 router.use('/users', users)
-router.use('/todos', todos)
-router.use('/', home)
+router.use('/todos', checkAuth, todos)
+router.use('/', checkAuth, home)
 
 module.exports = router
