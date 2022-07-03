beda8b75f9989fa9d9aab5ebfddf78a49ce0b658
Rita Chien <ritachien929@gmail.com>
2022-07-03 14:15:54 +0800

    feat: add create todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,6 +4,21 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// Add new todo
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const { name } = req.body
+
+    await Todo.create({ name, UserId })
+    return res.redirect('/')
+  } catch (err) { console.log(err) }
+})
+
 // Read: Todo's detail page
 router.get('/:id', async (req, res) => {
   const id = req.params.id
